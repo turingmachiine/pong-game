a65913807b30fd35d1d827f18edcdcdfd60ccd57
turingmachiine <strvtxv@gmail.com>
2019-12-17 21:55:47 +0300

    init commit

---
+++ src/sample/Main.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Pane root;
+    Pane startPane;
+    Pane gamePane;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
