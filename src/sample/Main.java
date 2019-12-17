3db6e5898803704dc1219da701518b5d234a5d6f
turingmachiine <strvtxv@gmail.com>
2019-12-17 22:01:37 +0300

    added start-game button

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,9 +15,36 @@ public class Main extends Application {
     Pane gamePane;
 
     @Override
-    public void start(Stage stage) throws Exception {
-
+    public void start(Stage stage) throws Exception{
+        root = new Pane();
+        root.setPrefSize(200, 200);
+        Scene scene = new Scene(root);
+        stage.setTitle("Navigator");
+        stage.setScene(scene);
+        initStartPane();
+        initGamePane();
+        root.getChildren().add(startPane);
+        stage.show();
     }
+
+
+    private void initStartPane() {
+        startPane = new Pane();
+        Button button = new Button("Начать игру");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+                root.getChildren().setAll(gamePane);
+            }
+        });
+        startPane.getChildren().add(button);
+    }
+
+    private void initGamePane() {
+        // game
+    }
+
+
     public static void main(String[] args) {
         launch(args);
     }
