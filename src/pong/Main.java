485ea747e518fc3e2be93fb4a54b1b52b6879ce7
turingmachiine <strvtxv@gmail.com>
2019-12-18 00:13:56 +0300

    added game with pc

--- src/pong/Main.java
+++ src/pong/Main.java
@@ -83,7 +83,35 @@ public class Main extends Application {
         context.setFill(Color.WHITE);
         context.fillRect(0, 0, Constants.WIDTH, Constants.HEIGHT);
         if (gameStarted) {
-
+            ballX += ballSpeedX;
+            ballY += ballSpeedY;
+            if (ballY + Constants.BALL_RADIUS > Constants.HEIGHT || ballY < 0) {
+                ballSpeedY *= -1;
+            }
+            if (ballX + Constants.BALL_RADIUS < 0) {
+                secondScroe += 1;
+                gameStarted = false;
+            } else if (ballX - Constants.BALL_RADIUS > Constants.WIDTH) {
+                firstScore += 1;
+                gameStarted = false;
+            } else if ((ballX + Constants.BALL_RADIUS >= Constants.WIDTH - Constants.PLAYER_WIDTH) && (ballY + Constants.BALL_RADIUS > playerTwoY) &&
+                    (ballY - Constants.BALL_RADIUS < playerTwoY + Constants.PLAYER_HEIGHT) || (ballX <= Constants.PLAYER_WIDTH) &&
+                    (ballY + Constants.BALL_RADIUS > playerOneY) && (ballY - Constants.BALL_RADIUS < playerOneY + Constants.PLAYER_HEIGHT)) {
+                if (Math.abs(ballSpeedX) < 8) {
+                    ballSpeedX += ballSpeedX > 0 ? 1 : - 1;
+                }
+                ballSpeedY = Math.abs(ballSpeedY) <= 1 ? ballSpeedY * (new Random().nextInt(2) + 1) :
+                        (int) Math.signum(1.0 * ballSpeedY);
+                ballSpeedX *= -1;
+            }
+            if(ballX < Constants.WIDTH - Constants.WIDTH  / 3) {
+                playerTwoY = ballY - Constants.PLAYER_HEIGHT / 2;
+            }  else {
+                int step = new Random().nextInt(2) + 1;
+                playerTwoY =  ballY > playerTwoY + Constants.PLAYER_HEIGHT / 2 ? playerTwoY + step: playerTwoY - step;
+            }
+            context.setFill(Color.BLACK);
+            context.fillOval(ballX, ballY, Constants.BALL_RADIUS, Constants.BALL_RADIUS);
         } else {
             context.setStroke(Color.BLACK);
             context.setTextAlign(TextAlignment.CENTER);
@@ -93,6 +121,14 @@ public class Main extends Application {
             ballSpeedX = new Random().nextInt(2) == 0 ? 1: -1;
             ballSpeedY = new Random().nextInt(2) == 0 ? 1: -1;
         }
+
+        context.setFill(Color.BLUEVIOLET);
+        context.fillRect(0, playerOneY, Constants.PLAYER_WIDTH, Constants.PLAYER_HEIGHT);
+        context.setFill(Color.DARKRED);
+        context.fillRect(Constants.WIDTH - Constants.PLAYER_WIDTH, playerTwoY, Constants.PLAYER_WIDTH,
+                Constants.PLAYER_HEIGHT);
+        context.setFill(Color.BLACK);
+        context.fillText("First " + firstScore + " - Second " + secondScroe, Constants.WIDTH / 4, 70);
     }
 
 
